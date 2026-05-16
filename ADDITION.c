9742d80e918b5c6c786bf7dc1eedf836ca08b28d
manasbajikar <manasbajikar@gmail.com>
2026-05-16 12:13:16 +0530

    Create ADDITION.c

---
+++ ADDITION.c
@@ -0,0 +1,14 @@
+#include &lt;stdio.h&gt;
+int main()
+{
+printf(&quot;Name: Manas V Bajikar\n&quot;);
+printf(&quot;USN: 1JS25CI053\n&quot;);
+printf(&quot;Program: Simple Addition of Two Numbers\n&quot;);
+printf(&quot;----------------------------------------\n&quot;);
+int a, b, sum;
+printf(&quot;Enter two numbers: &quot;);
+scanf(&quot;%d %d&quot;, &amp;a, &amp;b);
+sum = a + b;
+printf(&quot;Sum = %d\n&quot;, sum);
+return 0;
+}
